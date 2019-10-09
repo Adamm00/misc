a6dc20e1744bbe344cf660d34a65af5a211fb1bf
Adamm00 <28355318+Adamm00@users.noreply.github.com>
2019-10-10 04:25:16 +1000

    Merge with upstream

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ sudo true
 
 #######
 ### Asuswrt-Merlin.ng build script
-### Created By RMerlin, Modified By Adamm 9/03/2018
+### Created By RMerlin, Modified By Adamm 10/10/2019
 ### Expects you to have a copy of the sources at $SRC_LOC
 ### and model-specific copies as ~/amng.ac86, ~/amng.ac88, etc...
 ###
@@ -26,39 +26,24 @@ RSYNC_TREE=y
 # Run make clean before build
 #CLEANUP_TREE=y
 
-# Set DEFAULTBUILD to "none", then
-# uncomment desired models
-#
-# You can also set DEFAULTBUILD to one of the BUILDMODELS flags (see below)
 
-DEFAULTBUILD="none"
+### Uncomment the models you wish to build.
 
 #BAC56=y
-#BAC68=y
-#BAC87=y
 
-#BAC3200=y
-
-#BAC88=y
-#BAC3100=y
-#BAC5300=y
-
-#BAC86=y
+# BAC68=y
+# BAC87=y
+#
+# BAC3200=y
+#
+# BAC88=y
+# BAC3100=y
+# BAC5300=y
+#
+# BAC86=y
 
 BAX88=y
 
-### Specify at run time through env vars
-#
-# a) By group
-#    Example:
-# 	BUILDMODELS="sdk6|sdk7114" ./build-all
-#
-# Supported flags: all sdk6 sdk7x sdk7114 hnd mips arm none
-#
-# b) Or individual models (must disable group build):
-#
-#	BN66=y BUILDMODELS="none" ./build-all
-#
 
 ### Paths
 # Store built images there
@@ -67,53 +52,20 @@ STAGE_LOC=~/images
 # Copy built images there
 # Copy is done using scp, so it can be an ssh location
 #FINAL_LOC=""
+#FINAL_LOC=admin@router.asus.com:/mnt/sda1/Share
 FINAL_LOC=admin@192.168.1.69:/share/Storage/Firmware
 
 # Location of the original source code
+# SRC_LOC=~/amng
 SRC_LOC=~/asuswrt-merlin.ng
 
 ### End config
-
-
-
-if [ -z "$BUILDMODELS" ]; then
-	BUILDMODELS=$DEFAULTBUILD
-fi
-
-if [[ "$BUILDMODELS" =~ "arm" || "$BUILDMODELS" =~ "all" || "$BUILDMODELS" =~ "sdk6" ]] ; then
-	BAC56=y
-	BAC68=y
-	BAC87=y
-fi
-
-if [[ "$BUILDMODELS" =~ "arm" || "$BUILDMODELS" =~ "all" || "$BUILDMODELS" =~ "sdk7x" ]] ; then
-	BAC3200=y
-fi
-
-if [[ "$BUILDMODELS" =~ "arm" || "$BUILDMODELS" =~ "all" || "$BUILDMODELS" =~ "sdk7114" ]] ; then
-	BAC88=y
-	BAC3100=y
-	BAC5300=y
-fi
-
-if [[ "$BUILDMODELS" =~ "hnd" || "$BUILDMODELS" =~ "all" ]] ; then
-	BAC86=y
-fi
 
 
 build_fw()
 {
 	FWMODEL=$2
 	FWPATH=$1
-
-	if [ "$FWMODEL" == "rt-ax88u" ]; then
-		echo "*** $(date +%R) - Updating $FWMODEL branch..."
-		echo
-		cd ~/amng.ax88/ || exit 1
-		git checkout rtax88
-		git pull origin rtax88
-		echo
-	fi
 	echo "*** $(date +%R) - Starting building $FWMODEL..."
 	cd ~/"$FWPATH" || exit 1
 	make "$FWMODEL" &> output.txt
@@ -142,16 +94,24 @@ clean_tree()
 	FWPATH=$1
 	SDKPATH=$2
 	FWMODEL=$3
+	BRANCH=$4
+	echo "*** $(date +%R) - Cleaning up $FWMODEL..."
 	if [ "$RSYNC_TREE" == "y" ]; then
-		echo "*** $(date +%R) - Cloning $FWMODEL tree..."
+		echo "*** $(date +%R) - Updating $FWMODEL tree..."
 		sudo rsync -a --del $SRC_LOC/ ~/$FWPATH
+	fi
+	cd ~/$FWPATH || exit 1
+
+	CURRENT=$(git branch | grep \* | cut -d ' ' -f2)
+	if [ "$CURRENT" != "$BRANCH" ] ; then
+		git checkout $BRANCH
+		git pull origin rtax88
 	fi
 
 	if [ "$CLEANUP_TREE" == "y" ]; then
-		echo "*** $(date +%R) - Cleaning up $FWMODEL..."
 		cd ~/$FWPATH/$SDKPATH || exit 1
-		make cleankernel clean 2>/dev/null 2>&1
-		rm .config image/*.trx image/*.w 2>/dev/null
+		make cleankernel clean &>/dev/null 2>&1
+		rm .config image/*.trx image/*.w &>/dev/null
 	fi
 
 	echo -e "*** $(date +%R) - $FWMODEL code ready.\n"
@@ -167,46 +127,40 @@ else
 
 	echo "--- $(date +%R) - Global cleanup..."
 	mkdir -p $STAGE_LOC/backup
-	mv $STAGE_LOC/* $STAGE_LOC/backup/ 2>/dev/null
+	mv $STAGE_LOC/* $STAGE_LOC/backup/  2>/dev/null
 	cp $SRC_LOC/README-merlin.txt $STAGE_LOC/
 	cp $SRC_LOC/Changelog*.txt $STAGE_LOC/
 
 
 	# Update all model trees
 
-	echo "*** $(date +%R) - Updating Master branch..."
-	echo
-	cd $SRC_LOC || exit 1
-	git pull origin master
-	echo
-
 	echo "--- $(date +%R) - Preparing trees"
 	if [ "$BAC56" == "y" ]; then
-		clean_tree amng.ac56 release/src-rt-6.x.4708 rt-ac56u
+		clean_tree amng.ac56 release/src-rt-6.x.4708 rt-ac56u master
 	fi
 	if [ "$BAC68" == "y" ]; then
-		clean_tree amng.ac68 release/src-rt-6.x.4708 rt-ac68u
+		clean_tree amng.ac68 release/src-rt-6.x.4708 rt-ac68u mainline
 	fi
 	if [ "$BAC87" == "y" ]; then
-		clean_tree amng.ac87 release/src-rt-6.x.4708 rt-ac87u
+		clean_tree amng.ac87 release/src-rt-6.x.4708 rt-ac87u mainline
 	fi
 	if [ "$BAC3200" == "y" ]; then
-		clean_tree amng.ac3200 release/src-rt-7.x.main/src rt-ac3200
+		clean_tree amng.ac3200 release/src-rt-7.x.main/src rt-ac3200 mainline
 	fi
 	if [ "$BAC3100" == "y" ]; then
-		clean_tree amng.ac3100 release/src-rt-7.14.114.x/src rt-ac3100
+		clean_tree amng.ac3100 release/src-rt-7.14.114.x/src rt-ac3100 mainline
 	fi
 	if [ "$BAC88" == "y" ]; then
-		clean_tree amng.ac88 release/src-rt-7.14.114.x/src rt-ac88u
+		clean_tree amng.ac88 release/src-rt-7.14.114.x/src rt-ac88u mainline
 	fi
 	if [ "$BAC5300" == "y" ]; then
-		clean_tree amng.ac5300 release/src-rt-7.14.114.x/src rt-ac5300
+		clean_tree amng.ac5300 release/src-rt-7.14.114.x/src rt-ac5300 mainline
 	fi
 	if [ "$BAC86" == "y" ]; then
-		clean_tree amng.ac86 release/src-rt-5.02hnd rt-ac86u
+		clean_tree amng.ac86 release/src-rt-5.02hnd rt-ac86u mainline
 	fi
 	if [ "$BAX88" == "y" ]; then
-		clean_tree amng.ax88 release/src-rt-5.02axhnd rt-ax88u
+		clean_tree amng.ax88 release/src-rt-5.02axhnd rt-ax88u rtax88
 	fi
 
 	echo -e "--- $(date +%R) - All trees ready!\n"
@@ -247,8 +201,8 @@ else
 		sleep 10
 	fi
 	if [ "$BAX88" == "y" ]; then
-		build_fw amng.ax88/release/src-rt-5.02axhnd rt-ax88u &
-		sleep 10
+    build_fw amng.ax88/release/src-rt-5.02axhnd rt-ax88u &
+    sleep 10
 	fi
 
 	echo "--- $(date +%R) - All builds launched, please wait..."
@@ -257,7 +211,9 @@ else
 
 	echo ""
 	cd "$STAGE_LOC" || exit 1
-	sudo sha256sum *.w | unix2dos > sha256sums-ng.txt
+	# sha256sum *.trx | unix2dos > sha256sums-ng.txt
+	sha256sum *.w | unix2dos >> sha256sums-ng.txt
+
 
 	# Copy everything to the host
 
@@ -266,7 +222,6 @@ else
 	fi
 
 	git -C ~/amng.ax88 rev-parse HEAD > ~/Desktop/git.txt
-
 fi
 
 echo "=== $(date +%R) - All done!"
