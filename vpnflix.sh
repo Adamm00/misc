11b0ea8139a90ffd97b8bce79b39573b8b62f39f
Adamm00 <28355318+Adamm00@users.noreply.github.com>
2020-02-01 22:47:28 +1000

    Use new addon dir

--- vpnflix.sh
+++ vpnflix.sh
@@ -10,7 +10,7 @@
 #                                                                                   #
 #                      Route Netflix Traffic Thorugh VPN Client1                    #
 #                        By Adamm - https://github.com/Adamm00                      #
-#                                   29/12/2019                                      #
+#                                   01/02/2020                                      #
 #####################################################################################
 
 
@@ -58,14 +58,15 @@ case "$1" in
 
 	start)
 		Check_Lock "$@"
+		mkdir -p /jffs/addons/vpnflix
 		if [ ! -f "/jffs/scripts/firewall-start" ]; then
 			echo "#!/bin/sh" > /jffs/scripts/firewall-start
 		elif [ -f "/jffs/scripts/firewall-start" ] && ! head -1 /jffs/scripts/firewall-start | grep -qE "^#!/bin/sh"; then
 			sed -i '1s~^~#!/bin/sh\n~' /jffs/scripts/firewall-start
 		fi
-		cmdline="sh /jffs/scripts/vpnflix.sh start # VPNFlix"
-		if grep -E "sh /jffs/scripts/vpnflix.sh .* # VPNFlix" /jffs/scripts/firewall-start 2>/dev/null | grep -qvE "^#"; then
-			sed -i "s~sh /jffs/scripts/vpnflix.sh .* # VPNFlix .*~$cmdline~" /jffs/scripts/firewall-start
+		cmdline="sh /jffs/addons/vpnflix/vpnflix.sh start # VPNFlix"
+		if grep -E "sh /jffs/addons/vpnflix/vpnflix.sh .* # VPNFlix" /jffs/scripts/firewall-start 2>/dev/null | grep -qvE "^#"; then
+			sed -i "s~sh /jffs/addons/vpnflix/vpnflix.sh .* # VPNFlix .*~$cmdline~" /jffs/scripts/firewall-start
 		else
 			echo "$cmdline" >> /jffs/scripts/firewall-start
 		fi
@@ -73,12 +74,12 @@ case "$1" in
 			touch /jffs/configs/dnsmasq.conf.add
 		fi
 		if [ -d "/opt/bin" ] && [ ! -f "/opt/bin/vpnflix" ]; then
-			ln -s /jffs/scripts/vpnflix.sh /opt/bin/vpnflix
+			ln -s /jffs/addons/vpnflix/vpnflix.sh /opt/bin/vpnflix
 		fi
 		if [ "$(nvram get vpn_client1_state)" != "2" ]; then nvram set vpn_client1_state="2"; restartvpn="1"; fi
 		if [ -z "$(nvram get vpn_client_clientlist)" ]; then nvram set vpn_client_clientlist="<DummyVPN>172.16.1.1>0.0.0.0>VPN"; restartvpn="1"; fi
 		if [ -z "$(nvram get vpn_client1_clientlist)" ]; then nvram set vpn_client1_clientlist="<DummyVPN>172.16.1.1>0.0.0.0>VPN"; restartvpn="1"; fi
-		if [ -f "/jffs/scripts/vpnflix.ipset" ]; then ipset restore -! -f "/jffs/scripts/vpnflix.ipset"; fi
+		if [ -f "/jffs/addons/vpnflix/vpnflix.ipset" ]; then ipset restore -! -f "/jffs/addons/vpnflix/vpnflix.ipset"; fi
 		if ! ipset -L -n VPNFlix-Netflix >/dev/null 2>&1; then ipset -q create VPNFlix-Netflix hash:net timeout 604800; fi
 		if ! ipset -L -n VPNFlix-Other >/dev/null 2>&1; then ipset -q create VPNFlix-Other hash:net timeout 604800; fi
 		if ! ipset -L -n VPNFlix-Master >/dev/null 2>&1; then ipset -q create VPNFlix-Master list:set; ipset -q -A VPNFlix-Master VPNFlix-Netflix; ipset -q -A VPNFlix-Master VPNFlix-Other; fi
@@ -94,14 +95,14 @@ case "$1" in
 		iptables -A PREROUTING -t mangle -i tun21 -m set --match-set VPNFlix-Master dst -j MARK --set-xmark "$FWMARK_OVPNC1" 2>/dev/null
 		Populate_Config
 		cru d VPNFlix_save
-		cru a VPNFlix_save "30 * * * * sh /jffs/scripts/vpnflix.sh save"
+		cru a VPNFlix_save "30 * * * * sh /jffs/addons/vpnflix/vpnflix.sh save"
 		if [ "$restartvpn" = "1" ]; then nvram commit; service "restart_vpnclient1"; fi
 	;;
 	save)
 		Check_Lock "$@"
 		echo "Saving VPNFlix Server List..."
-		if ipset -L -n VPNFlix-Master >/dev/null 2>&1; then { ipset save VPNFlix-Netflix; ipset save VPNFlix-Other; ipset save VPNFlix-Master; } > "/jffs/scripts/vpnflix.ipset" 2>/dev/null; fi
-		echo "Complete! - $(wc -l < /jffs/scripts/vpnflix.ipset) Entries Total"
+		if ipset -L -n VPNFlix-Master >/dev/null 2>&1; then { ipset save VPNFlix-Netflix; ipset save VPNFlix-Other; ipset save VPNFlix-Master; } > "/jffs/addons/vpnflix/vpnflix.ipset" 2>/dev/null; fi
+		echo "Complete! - $(wc -l < /jffs/addons/vpnflix/vpnflix.ipset) Entries Total"
 	;;
 	disable)
 		Check_Lock "$@"
@@ -111,7 +112,7 @@ case "$1" in
 		iptables -D PREROUTING -t mangle -m set --match-set VPNFlix-Master dst -j MARK --set-mark "$FWMARK_OVPNC1" 2>/dev/null
 		iptables -D POSTROUTING -t nat -s "$(nvram get vpn_server1_sn)"/24 -o tun11 -j MASQUERADE 2>/dev/null
 		iptables -D PREROUTING -t mangle -i tun21 -m set --match-set VPNFlix-Master dst -j MARK --set-xmark "$FWMARK_OVPNC1" 2>/dev/null
-		if ipset -L -n VPNFlix-Master >/dev/null 2>&1; then { ipset save VPNFlix-Netflix; ipset save VPNFlix-Other; ipset save VPNFlix-Master; } > "/jffs/scripts/vpnflix.ipset" 2>/dev/null; fi
+		if ipset -L -n VPNFlix-Master >/dev/null 2>&1; then { ipset save VPNFlix-Netflix; ipset save VPNFlix-Other; ipset save VPNFlix-Master; } > "/jffs/addons/vpnflix/vpnflix.ipset" 2>/dev/null; fi
 		ipset destroy VPNFlix-Master
 		ipset destroy VPNFlix-Netflix
 		ipset destroy VPNFlix-Other
@@ -127,7 +128,7 @@ case "$1" in
 		ipset destroy VPNFlix-Master
 		ipset destroy VPNFlix-Netflix
 		ipset destroy VPNFlix-Other
-		rm -rf /jffs/scripts/vpnflix.ipset
+		rm -rf /jffs/addons/vpnflix
 		echo "Complete!"
 	;;
 	*)
